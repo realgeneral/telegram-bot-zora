9c470fe34899bfb811ce4b35c24fcb886c14ed1b
artemtret <artem.tretykov74@gmail.com>
2023-08-11 17:34:02 +0300

    add for prem users more slots

--- app/handlers/start_cmd.py
+++ app/handlers/start_cmd.py
@@ -6,6 +6,7 @@ from aiogram.dispatcher import FSMContext
 from app.create_bot import dp, bot
 from app.states import UserFollowing
 from app.keyboards import check_sub_menu
+from app.handlers import admin
 
 CHANNEL_ID = -1001984019900
 NOTSUB_MESSAGE = "Looks like you're not subscribed yet! 🙁 Subscribe now to access all the features"
@@ -70,9 +71,12 @@ async def is_subscribe(callback_query: types.CallbackQuery):
     if check_sub_channel(await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=callback_query.from_user.id)):
         await UserFollowing.get_private_keys.set()
         await bot.send_message(callback_query.from_user.id, "👝 *Submit your private key's* \n\n"
+                                                            "_Free version_: up to 10 keys.\n "
+                                                            "_Premium version_: up to 50 keys. \n"
+                                                            "For access to the premium version, please contact us. \n\n"
                                                             "_The bot doesn't collect or store your personal data or "
                                                             "private keys. "
-                                                            "Zora bot — fully open source project. \n "
+                                                            "Zora bot — fully open source project. \n\n "
                                                             "GitHub: https://github.com/realgeneral/telegram-bot-zora_",
                                parse_mode=types.ParseMode.MARKDOWN,
                                reply_markup=ReplyKeyboardRemove())
@@ -82,7 +86,12 @@ async def is_subscribe(callback_query: types.CallbackQuery):
 
 @dp.message_handler(state=UserFollowing.get_private_keys)
 async def private_keys(message: types.Message, state: FSMContext):
-    private_keys = message.text.split('\n')
+    if int(message.from_user.id) in admin.list_of_prem_users:
+        private_keys = message.text.split('\n')[:50]
+        message_response = "😌 *Keys saved successfully (max. 50) *"
+    else:
+        private_keys = message.text.split('\n')[:10]
+        message_response = "😌 *Keys saved successfully (max. 10) *"
     await state.update_data(private_keys=private_keys)
 
     buttons = [
@@ -92,6 +101,6 @@ async def private_keys(message: types.Message, state: FSMContext):
                                        resize_keyboard=True)
 
     await UserFollowing.wallet_menu.set(),
-    await message.answer("😌 *Keys saved successfully*",
+    await message.answer(message_response,
                          parse_mode=types.ParseMode.MARKDOWN,
                          reply_markup=reply_markup)
