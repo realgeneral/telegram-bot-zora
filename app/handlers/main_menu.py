5f2ead57a72eb0fea72f55b0b4b60896b628736e
artemtret <artem.tretykov74@gmail.com>
2023-08-11 15:45:25 +0300

    refactor keyboards

--- app/handlers/main_menu.py
+++ app/handlers/main_menu.py
@@ -16,18 +16,20 @@ async def go_menu(message: types.Message, state: FSMContext):
 @dp.message_handler(state=UserFollowing.wallet_menu)
 async def send_menu(message: types.Message, state: FSMContext):
     data = await state.get_data()
-    private_key = data.get("private_key")
+    private_keys = list(data.get("private_keys"))
+    message_response = "# *Private key`s* \n"
+    for i in range(len(private_keys)):
+        message_response += f"{i+1}. *{private_keys[i][0:6]}...{private_keys[i][-4:]}* \n"
+    message_response += "\n Choose an action:"
 
     b1 = KeyboardButton("👝 Check balance")
-    b2 = KeyboardButton("⛏ Mint nft")
-    b3 = KeyboardButton("💸 Bridge")
-    b4 = KeyboardButton("🆕 New keys")
-    b5 = KeyboardButton("🔑 Check keys")
+    b2 = KeyboardButton("💸 Tap 2 earn")
+    b3 = KeyboardButton("🆕 New keys")
+    b4 = KeyboardButton("🔑 Check keys")
 
     buttons = ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons.row(b1).row(b2, b3).row(b4, b5)
+    buttons.row(b1).row(b2, b4).row(b3)
 
     await UserFollowing.choose_point.set()
-    await message.answer(f"# Private key *{private_key[0:6]}...{private_key[-4:]}* \n"
-                         f"Choose an action:", parse_mode=types.ParseMode.MARKDOWN,
+    await message.answer(message_response, parse_mode=types.ParseMode.MARKDOWN,
                          reply_markup=buttons)
