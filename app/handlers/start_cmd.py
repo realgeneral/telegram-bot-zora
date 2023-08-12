20f003954d9a4cd33be0699733a1ddf8147782d7
artemtret <artem.tretykov74@gmail.com>
2023-08-12 18:40:39 +0300

    edit message

--- app/handlers/start_cmd.py
+++ app/handlers/start_cmd.py
@@ -15,36 +15,16 @@ NOTSUB_MESSAGE = "Looks like you're not subscribed yet! 🙁 Subscribe now to ac
 @dp.message_handler(commands=['start'])
 async def start_cmd(message: types.Message):
     buttons = [
-        KeyboardButton(text="🚀 Start"),
+        KeyboardButton(text="🤿 Alright, let's dive in!"),
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard=[buttons], resize_keyboard=True)
 
     await UserFollowing.check_subscribe.set()
-    await message.answer("Welcome to the <b>Zora Automatization</b> bot! 🤖👋 \n\n"
-                         # "📍 Рекомендую ознакомиться с <a href='https://t.me/trioinweb3/13'>гайдом</a> 📍\n\n"
-                         "The bot execute:\n",
-                         # "<u> 1. Минт nft: </u> \n"
-                         # "  🔘 <i> Greenfield Testnet </i> (на BNB Chain) \n"
-                         # "  🔘 <i> ZkLightClient </i> (на BNB Chain) \n"
-                         # "  🔘 <i> ZkBridge on opBNB </i> (на BNB Chain) \n"
-                         # "  🔘 <i> Mainnet Alpha </i> (на Core) \n"
-                         # "  🔘 <i> Pandra </i> (на BNB Chain, Polygon, Core, Celo) \n\n"
-                         # "<u> 2. Кроссчейн nft сендер (zknft): </u> \n"
-                         # '  🔘 <i> ZkLightClient nft </i> из BSC в opBNB \n'
-                         # '  🔘 <i> ZkBridge on opBNB nft </i> из BSC в opBNB \n'
-                         # '  🔘 <i> Mainnet Alpha nft </i> из Core в Polygon \n'
-                         # '  🔘 <i> CodeConqueror (Pandra) nft </i> из BSC в Core \n'
-                         # '  🔘 <i> PixelProwler (Pandra) nft </i> из Polygon в BSC \n'
-                         # '  🔘 <i> MelodyMaven (Pandra) nft </i> из Core в Polygon \n'
-                         # '  🔘 <i> EcoGuardian (Pandra) nft </i> из Celo в BSC \n\n'
-                         # '<u> 3. Отправка сообщений (zkMessenger): </u> \n'
-                         # '  🔘 <i> из BSC в Polygon </i> \n\n'
-                         # ''
-                         # '<u>Необходимое количество токенов для оплаты газа </u> \n'
-                         # ' BNB = <i>0.02065</i> \n'
-                         # ' Matic = <i>3.1</i> \n'
-                         # ' Core = <i>2.5</i> \n'
-                         # ' Celo = <i>5</i>',
+    await message.answer(f"Hey there, <b> {message.from_user.first_name} </b>, right place, right time! \n \n"
+                         "Our Zora Bot benefits:\n"
+                         "  🔘 🆓 It's free \n"
+                         "  🔘 🌐 Open source \n"
+                         "  🔘 😃 User-friendly \n",
                          parse_mode=types.ParseMode.HTML,
                          reply_markup=reply_markup)
 
@@ -55,12 +35,13 @@ def check_sub_channel(chat_member):
     return False
 
 
-@dp.message_handler(Text(equals="🚀 Start"), state=UserFollowing.check_subscribe)
+@dp.message_handler(Text(equals="🤿 Alright, let's dive in!"), state=UserFollowing.check_subscribe)
 async def check_subscribe(message: types.Message):
-
     await UserFollowing.check_subscribe.set()
-    await message.answer("👋📢 To enjoy the full features of our bot, kindly subscribe to our "
-                         "<a href='https://t.me/trioinweb3'>channel</a> first",
+    await message.answer("👋📢 Whoa, hold up! Haven't joined our <a href='https://t.me/trioinweb3'>channel</a> yet? \n"
+                         "We're dropping crypto wisdom and sharing our know-how. \n"
+                         "Your sub supports us to make new cool bots for you! \n \n"
+                         "Hit that sub button below, then hit us back with 'Done '! ",
                          parse_mode=types.ParseMode.HTML,
                          reply_markup=check_sub_menu)
 
@@ -70,15 +51,17 @@ async def is_subscribe(callback_query: types.CallbackQuery):
     await bot.delete_message(chat_id=callback_query.message.chat.id, message_id=callback_query.message.message_id)
     if check_sub_channel(await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=callback_query.from_user.id)):
         await UserFollowing.get_private_keys.set()
-        await bot.send_message(callback_query.from_user.id, "👝 *Submit your private keys* \n\n"
-                                                            "_Free version_: up to 10 keys.\n"
-                                                            "_Premium version_: up to 50 keys. \n"
-                                                            "For access to the premium version, please contact us. \n\n"
-                                                            "_The bot doesn't collect or store your personal data or "
+        await bot.send_message(callback_query.from_user.id, "👝 <b> Submit your private keys </b> \n\n"
+                                                            "<i> Free version </i>: up to 10 keys.\n"
+                                                            "<i> Premium version </i>: up to 50 keys. \n"
+                                                            "For access to the premium version, please "
+                                                            "<a href='https://t.me/whatheshark'>contact us </a>. \n\n"
+                                                            "<i> The bot doesn't collect or store your personal data or"
                                                             "private keys. "
                                                             "Zora bot — fully open source project. \n\n "
-                                                            "GitHub: https://github.com/realgeneral/telegram-bot-zora_",
-                               parse_mode=types.ParseMode.MARKDOWN,
+                                                            "GitHub: https://github.com/realgeneral/telegram-bot-zora "
+                                                            "</i>",
+                               parse_mode=types.ParseMode.HTML,
                                reply_markup=ReplyKeyboardRemove())
     else:
         await bot.send_message(callback_query.from_user.id, NOTSUB_MESSAGE, reply_markup=check_sub_menu)
