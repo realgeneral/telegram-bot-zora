ce283fdb31b6c2e101eec0a5f299f79c18f86da3
artemtret <artem.tretykov74@gmail.com>
2023-09-05 20:58:03 +0300

    logger config update

--- app/logs/log.py
+++ app/logs/log.py
@@ -1,12 +1,14 @@
+import inspect
+
 from loguru import logger as logging
+
 
 logging.add("logs/logs.log",
             rotation="500 MB",
             backtrace=True,
             diagnose=True,
-            # pylint: disable=line-too-long
-            format="<yellow>{time:YYYY-MM-DD at HH:mm:ss}</yellow> | <level>{level}</level>: <level>{message}</level>"
-            # pylint: enable=line-too-long
+            format="<green>{time:YYYY-MM-DD at HH:mm:ss}</green> | <level>{level}</level> | <level>{function}</level> : <level>{message}</level>",
+            colorize=False  # Включаем цветовую разметку
             )
-logging = logging.opt(colors=True)
+logging.opt(colors=True)
 
