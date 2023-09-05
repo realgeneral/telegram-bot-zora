ab5be300c74049efeadc76abccc4159ed5e80743
artemtret <artem.tretykov74@gmail.com>
2023-09-05 21:00:07 +0300

    restart adding  and Back to menu button update

--- app/handlers/main_menu.py
+++ app/handlers/main_menu.py
@@ -1,24 +1,39 @@
 from aiogram import types
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from aiogram.dispatcher.filters import Text
 from aiogram.dispatcher import FSMContext
 
 from app.create_bot import dp
 from app.states import UserFollowing
+from app.utils.Bridger import Bridger
 
 
-@dp.message_handler(Text(equals=["⬅ Go to menu"]), state='*')
+@dp.message_handler(Text(equals=["⬅ Go to menu", "/restart"]), state='*')
 async def go_menu(message: types.Message, state: FSMContext):
+    random_amount = []
+
     current_state = await state.get_state()
+
     if current_state == UserFollowing.tap_to_earn.state:
+        print(state)
         await state.update_data(stop_flag=True)
 
     data = await state.get_data()
     private_keys = data.get("private_keys")
+    bridge_amount = data.get("random_amount")
 
     if private_keys is None:
         private_keys = ["-"]
         await state.update_data(private_keys=private_keys)
+
+        for _ in private_keys:
+            random_amount.append(Bridger.choose_random_amount(0.009501, 0.01003))
+        await state.update_data(random_amount=random_amount)
+
+    if bridge_amount is None and len(random_amount) == 0:
+        for _ in private_keys:
+            random_amount.append(Bridger.choose_random_amount(0.009501, 0.01003))
+        await state.update_data(random_amount=random_amount)
 
     await UserFollowing.wallet_menu.set()
     await send_menu(message)
@@ -43,3 +58,5 @@ async def send_menu(message: types.Message):
     await UserFollowing.choose_point.set()
     await message.answer(message_response, parse_mode=types.ParseMode.MARKDOWN,
                          reply_markup=buttons)
+
+
---
+++ app/handlers/restart.py
@@ -0,0 +1,12 @@
+from aiogram import types
+from aiogram.types import ReplyKeyboardRemove
+from aiogram.dispatcher import FSMContext
+
+from app.create_bot import dp
+
+
+@dp.message_handler(state='*')
+async def restart_cmd(message: types.Message):
+    await message.answer(f"😊 _Try /restart_",
+                         parse_mode=types.ParseMode.MARKDOWN,
+                         reply_markup=ReplyKeyboardRemove())
